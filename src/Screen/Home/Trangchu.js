ef2b68fc6112badad530d4cee093e53b89e14d36
ChanhHieu <chanhhieu@hoalusport.com>
2017-12-13 00:02:36 +0700

    ui ngay12

--- src/Screen/Home/Trangchu.js
+++ src/Screen/Home/Trangchu.js
@@ -18,6 +18,7 @@ export default class Trangchu extends Component {
     return (
       <Container style={styles.container}>
         <Content>
+            {/* swiper */}
           <View style = {{height:height/4}} >
             <SwiperFlatList
               autoplay
@@ -40,98 +41,140 @@ export default class Trangchu extends Component {
               </View>
             </SwiperFlatList>
           </View>
+          
             {/* cà phê cuối tuần*/}
-            <View>
-              <Text style = {styles.textcfcuoituan}>CÀ PHÊ CUỐI TUẦN</Text>
-              <Text style = {styles.textchunhat}>Ngày chủ nhật nhâm nhi tách cà phê cùng bạn bè</Text>
-            </View>
+          <View>
+            <Text style = {styles.textcfcuoituan}>CÀ PHÊ CUỐI TUẦN</Text>
+            <Text style = {styles.textchunhat}>Ngày chủ nhật nhâm nhi tách cà phê cùng bạn bè</Text>
+          </View>
 
             {/* 3 ảnh ghép chung */}
-            <View style = {styles.imagecoffee}>
-                {/* ảnh1 */}
-              <TouchableOpacity>
-                  {/* background image cafe1 */}
-                <View style = {{position: 'relative'}}>
-                  <Image  
-                    source = {require('../../img/coffee1.png')} 
-                    style ={styles.imgcoffee1}>
-                  </Image>
-                </View>
-                  {/* thumbail img cafe */}
-                <View>
-                  <Image 
-                    style = {{width: 70, height: 70, position:'absolute', borderColor:'transparent', borderRadius:5, left:10, bottom: 10}}
-                    source = {require('../../img/cafe-logo-1.png')}/>
-                  <Text style = {{color:'white',marginLeft: 80,position:'absolute', bottom: 10, left:10}}>Cà Phê Cùng Bạn Bè</Text>
-                  <Image style = {{position:'absolute', bottom: 10, right:10}} source = {require('../../img/play-button.png')}/>
-                </View>
-              </TouchableOpacity>
-              
+          <View style = {styles.imagecoffee}>
+              {/* ảnh1 */}
+            <TouchableOpacity>
+                {/* background image cafe1 */}
+              <View style = {{position: 'relative'}}>
+                <Image  
+                  source = {require('../../img/coffee1.png')} 
+                  style ={styles.imgcoffee1}>
+                </Image>
+              </View>
+                {/* thumbail img cafe */}
+              <View>
+                <Image 
+                  style = {styles.thumbnail_cf}
+                  source = {require('../../img/caphe1minh.jpg')}/>
+                <Text style = {styles.text1_cfcuoituan}>Cà Phê Cùng Bạn Bè</Text>
+                <Image style = {styles.img_cfcuoituan} source = {require('../../img/play-icon.png')}/>
+              </View>
+            </TouchableOpacity>
+            
               {/* ảnh2 */}
-              <TouchableOpacity>
-                <View>
-                  <Image 
-                    source = {require('../../img/coffee2.png')} 
-                    style ={styles.imgcoffee2}>
-                  </Image>
-                </View>
-              </TouchableOpacity>
-
-              {/* ảnh3 */}
-              <TouchableOpacity>
-                <View>
-                  <Image
-                    source = {require('../../img/coffee3.jpg')} 
-                    style ={styles.imgcoffee3}>
-                  </Image>
-                </View>
-              </TouchableOpacity>
-
-              {/* xem thêm playlist  */}
-              <TouchableOpacity >
-                <View style = {{flexDirection:'row', padding: 10}}>
-                  <Image source = {require('../../img/4block.png')}/> 
-                  <Text style = {{marginLeft: 10}}>Xem thêm playlist</Text>
-                </View>
-              </TouchableOpacity>
-
-              {/* zing chat real-time */}
-              <View style = {{backgroundColor:'black', height:height/4}}>
-              </View>
-
-              {/* 3 ca sĩ */}
-              <View style = {{width, height:80, flexDirection:'row', marginTop: 20}}>
-                <View style = {styles.threecasi}>
-                  <Image source = {require('../../img/baoanh.jpeg')}
-                     style = {{width:60, height:60}}/>
-                  <Text>Bảo Anh</Text>
-                </View>
-                <View>
-                  <Image source = {require('../../img/phuocthinh.jpg')} 
-                    style = {{width:60, height:60}}/>
-                  <Text>Noo Phước Thịnh</Text>
-                </View>
-                <View>
-                  <Image source = {require('../../img/jusstatee.jpg')}
-                    style = {{width:60, height:60}}/>
-                  <Text>JusstaTee</Text>
-                </View>
-              </View>
-              
-              {/* thống kê ngày */}
+            <TouchableOpacity>
               <View>
-                <TouchableOpacity >
-                  <View style = {{flexDirection:'row', padding: 10}}>
-                    <Image source = {require('../../img/4block.png')}/> 
-                    <Text style = {{marginLeft: 10}}>26.11.2017 - 14:00</Text>
-                  </View>
-                </TouchableOpacity>
+                <Image 
+                  source = {require('../../img/coffee2.png')} 
+                  style ={styles.imgcoffee2}>
+                </Image>
               </View>
+                    {/* thumbail img cafe */}
               <View>
-
+                <Image 
+                  style = {styles.thumbnail_cf}
+                  source = {require('../../img/caphevoibanbe.jpg')}/>
+                <Text style = {styles.text1_cfcuoituan}>Cà Phê Cùng Nhạc Phim</Text>
+                <Image style = {styles.img_cfcuoituan} source = {require('../../img/play-icon.png')}/>
               </View>
+            </TouchableOpacity>
+
+                {/* ảnh3 */}
+            <TouchableOpacity>
+              <View>
+                <Image
+                  source = {require('../../img/coffee3.jpg')} 
+                  style ={styles.imgcoffee3}>
+                </Image>
+              </View>
+                {/* thumbail img cafe */}
+              <View>
+                <Image 
+                  style = {styles.thumbnail_cf}
+                  source = {require('../../img/thuhanoi.jpg')}/>
+                <Text style = {styles.text1_cfcuoituan}>Mùa Thu Hà Nội</Text>
+                <Image style = {styles.img_cfcuoituan} source = {require('../../img/play-icon.png')}/>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+            {/* xem thêm playlist  */}
+          <TouchableOpacity>
+            <View style = {styles.view_xemthemplaylist}>
+              <Image source = {require('../../img/4block.png')}/> 
+              <Text style = {styles.text_xemthemplaylist}>Xem thêm playlist</Text>
             </View>
+          </TouchableOpacity>
 
+            {/* zing chat real-time */}
+          <View>
+            <View style = {styles.view_zingChatRealTime}>
+              <Text style = {styles.text1_chatRT}>ZING CHAT REAL-TIME</Text>
+              <TouchableOpacity>
+                 <Image style = {styles.img_playzingchatRT} source = {require('../../img/play-icon.png')}/>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text style = {{marginLeft:70}}>Xem thêm</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style = {{height:height/4, width, backgroundColor:'black',marginRight: 7.5,marginLeft:7.5}}></View>
+          
+            {/* 3 ca sĩ */}
+          <View >
+            <View style = {styles.view_3Casi}>
+              <View style = {styles.view_3Casi_all}>
+                <Image source = {require('../../img/baoanh.jpeg')}
+                  style = {styles.imgCaSi}/>
+                <Text style = {styles.text_TenCaSi}>Bảo Anh</Text>
+              </View>
+              <View  style = {styles.view_3Casi_all}>
+                <Image source = {require('../../img/phuocthinh.jpg')} 
+                  style = {styles.imgCaSi}/>
+                <Text style = {styles.text_TenCaSi}>Noo Phước Thịnh</Text>
+              </View>
+              <View style = {styles.view_3Casi_all}>
+                <Image source = {require('../../img/jusstatee.jpg')}
+                  style = {styles.imgCaSi}/>
+                <Text style = {styles.text_TenCaSi}>JusstaTee</Text>
+              </View>
+            </View> 
+          </View>
+
+          {/* sing of JusstaTee */}
+          <TouchableOpacity >
+            <View style = {{position:'relative'}}>
+              <Image
+                source = {require('../../img/back-jusstatee.jpg')}
+                style = {styles.img_Back_Jusstatee}
+              />
+            </View>
+            <View style = {{position:'absolute', flexDirection:'row'}}>
+              <Image 
+                style = {styles.thumbnail_jussta}
+                source = {require('../../img/jussta-thumb.jpg')}/>
+                <View style = {{marginLeft:7}}>
+                  <Text style  = {styles.txt_justa} >Đã lỡ yêu em nhiều</Text>
+                  <Text style  = {styles.txt_justa}>JusstaTee</Text>
+                  <Text style  = {styles.txt_justa}>Chạm đỉnh Zing: 0 lần</Text>
+                </View>
+            </View>
+          </TouchableOpacity>
+
+           {/* thống kê ngày */}
+           <View>
+              <TouchableOpacity >
+                  <Text style = {{marginLeft: 10}}>26.11.2017 - 14:00</Text>
+              </TouchableOpacity>
+            </View>
         </Content>
       </Container>
     );
--- src/Screen/Home/styles.js
+++ src/Screen/Home/styles.js
@@ -35,7 +35,8 @@ const styles = ({
     height:100,
     borderColor:'grey',
     borderWidth:0.1,
-    marginTop: 2, marginBottom:2
+    marginTop: 2, 
+    marginBottom:2
    },
    imgcoffee3:{
     width, 
@@ -53,6 +54,93 @@ const styles = ({
    },
    textchunhat:{
     textAlign:'center',
+   },
+   thumbnail_cf:{
+    width: 50, 
+    height: 50, 
+    position:'absolute', 
+    borderColor:'transparent', 
+    borderRadius:5, 
+    left:10, 
+    bottom: 10
+   },
+   text1_cfcuoituan:{
+    color:'white',
+    marginLeft: 60,
+    position:'absolute', 
+    bottom: 10, left:10
+   },
+   img_cfcuoituan:{
+    position:'absolute', 
+    bottom: 10, 
+    right:10
+   },
+   view_xemthemplaylist:{
+    flexDirection:'row', 
+    padding: 10,
+    marginRight:7.5,
+    marginLeft:7.5
+   },
+   text_xemthemplaylist:{
+    marginLeft: 10
+   },
+   view_zingChatRealTime:{
+   position:'relative',
+    marginLeft:7.5, 
+    marginRight:7.5,
+    flexDirection:'row',
+    marginBottom:5
+   },
+   view_3Casi:{
+    marginLeft:7.5,
+    marginRight:7.5,
+    height:90, 
+    flexDirection:'row', 
+    marginTop: 20, 
+    alignItems:'flex-end',
+   },
+   view_3Casi_all:{
+     flex:1/3,
+     justifyContent:'center',
+     alignItems:'center'
+   },
+   text_TenCaSi:{
+     fontSize:13,
+     marginTop:5
+   },
+   imgCaSi:{
+     height:60,
+     width:60
+   },
+   img_playzingchatRT:{
+     marginLeft:10
+   },
+   text1_chatRT:{
+     fontWeight:'bold',
+     fontSize:18,
+   },
+   img_Back_Jusstatee:{
+    width, 
+    height:100,
+    borderColor:'grey',
+    borderWidth:0.1,
+    borderRadius:5,
+    marginRight:7.5
+   },
+   view_Sing_JT:{
+    position:'relative'
+   },
+   thumbnail_jussta:{
+    width: 60, 
+    height: 60, 
+    borderColor:'transparent', 
+    borderRadius:5,
+    alignItems:'center',
+    justifyContent:'center' 
+   },
+   txt_justa:{
+     color:'white',
+     fontSize:13,
    }
 });
   export default styles;
